458768925f1ef827a75e5c54df5b02606b1159bf
Ana Hobden <operator@hoverbear.org>
2019-08-26 16:55:28 -0700

    Touchups
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- scripts/build-deps-for-docker.sh
+++ scripts/build-deps-for-docker.sh
@@ -4,39 +4,30 @@ set +e
 # Support building the dependencies for a docker build in a separate step.
 # This script should be run with `pwd` being the `tikv` repo.
 
-# This script exists because of https://github.com/rust-lang/cargo/issues/2644 and 
-# `[replace]` doesn't play well with `cargo vendor`.
-# This is complicated by https://github.com/rust-lang/cargo/issues/7267
-
-# The general idea is:
-# 
+# The general idea is: 
 # Stub out `/src/lib.rs` for every component and let the dependencies build. Then you can recopy the source and run the build again.
 # This build should then be able to only need to build TiKV components.
-
-# TODO: Why do we need to grep out `test` and `profiler`?
-components=$(ls -d ./components/* | xargs -n 1 basename | grep -v "test" | grep -v "profiler")
-# We must remove the profiler from tidb_query. (TODO: Why do we need to grep out `test` and `profiler`?)
-sed -i '/profiler/d' ./components/tidb_query/Cargo.toml
+components=$(ls -d ./components/* | xargs -n 1 basename | grep -v "test")
+sed -i '/fuzz/d' ./Cargo.toml
+sed -i '/test\_/d' ./Cargo.toml
 
 rm -rf ./src
 mkdir -p ./src
 echo '' > ./src/lib.rs
 for component in ${components}; do
-    rm -rf ./components/${component}/src/
+    echo ${component}
     mkdir -p ./components/${component}/src/
     echo '' > ./components/${component}/src/lib.rs
 done
+mkdir -p ./components/profiler/examples/
+echo 'fn main() {}' > ./components/profiler/examples/prime.rs
 
 # TODO: Unclear why not in `components/`
 rm -rf ./cmd/src
-mkdir -p ./cmd/src/bin/
+mkdir -p ./cmd/src/bin
 echo 'fn main() {}' > ./cmd/src/bin/tikv-ctl.rs
 echo 'fn main() {}' > ./cmd/src/bin/tikv-server.rs
 echo '' > ./cmd/src/lib.rs
-rm -rf ./fuzz/src
-mkdir -p ./fuzz/src/
-echo '' > ./fuzz/src/lib.rs
-
 
 make build_dist_release
 
